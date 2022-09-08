adf32d2d0464dcefeaba1ce72606a5125f5aa383
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-09-08 17:59:17 +1000

    first commit

---
+++ public/app.js
@@ -0,0 +1,40 @@
+let files = [];
+let loaded;
+
+$(document).ready(function() {
+function preloadImage(url) {
+    var img=new Image();
+    img.src=url;    
+};
+
+fileNames = [];
+preloadImage();
+
+// can use fetch to get images from back end to front end, .fetch is a browser api
+
+// display them on fullscreen
+
+// change on keypress
+
+fetch("http://localhost:8080/images")
+.then((res) => res.json())
+.then((data) => {
+    files = data.files;
+    // event listener is files.next (find index for whatever the current img is)
+    $('#the-img').attr("src", `/assets/${data.files[0]}`)
+});
+
+$('body').on('keydown', (e) => {
+    const currentIndex = files.indexOf($("#the-img").attr("src").replace('/assets/', ''));
+    $("#the-img").attr('src', `/assets/${files[currentIndex+1]}`);
+    console.log(`${files[currentIndex+1]}`)
+}); 
+// get the src from the image on screen then remove the folder stuff so it's just the file name like in the array and use that to find the current index
+
+
+
+
+// no delete
+});
+
+
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs/promises');
+const path = require('path');
+const app = express();
+const port = 8080;
+//
+app.use(express.static('public'))
+
+const directoryPath = path.join(__dirname, 'public/assets');
+
+app.get('/images', (req, res) => {
+    fs.readdir(directoryPath).then(function(files) {
+        res.json({files});
+    }).catch((err) => {
+        if (err) {
+            return console.log('oh no, an error', err);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+});
